adbead7438379cac302c54a67bab905b8c018261
Ricardo Leme <ricardo.leme@gmail.com>
2021-11-08 11:57:42 -0300

    New port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 const app = express();
-const port = 80
+const port = process.env.PORT || 4000
 
 app.use(cors()) //Habilita o CORS-Cross-origin resource sharing
 app.use(express.urlencoded({extended: true}));
