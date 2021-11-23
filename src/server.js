372be90ca3122b21755fdf824cbf226e2e6fd679
Ricardo Leme <ricardo.leme@gmail.com>
2021-11-23 11:10:24 -0300

    Improved message

--- src/server.js
+++ src/server.js
@@ -6,21 +6,21 @@ const app = express();
 const port = process.env.PORT || 4000
 
 app.use(cors()) //Habilita o CORS-Cross-origin resource sharing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()) // Parse JSON payloads
 app.use('/favicon.ico', express.static('public/favicon.ico')) //Configura o favicon
 app.disable('x-powered-by') //Removendo o x-powered-by por segurança
 
 // Rotas do conteúdo público 
-app.use('/', express.static('public')) 
+app.use('/', express.static('public'))
 
 //Definimos a nossa rota default
 app.get('/api', (req, res) => {
   res.status(200).json({
-      message: 'API Dog Walker - 100% funcional!🐕👏',
-      version: '1.0.1'
+    message: 'API Dog Walker - 100% funcional!🐕👏',
+    version: '1.0.1'
   })
-  
+
 })
 
 //Rotas dos Usuários
@@ -30,10 +30,17 @@ app.use('/api/usuarios', rotasUsuarios)
 //app.use('/upload', rotaUpload)
 
 // Rota para tratar exceções - 404 (Deve ser a última rota SEMPRE) 
-app.use(function(req, res) {
-  res.status(404).json(
-      {message: `A rota ${req.originalUrl} não existe 🚫`}
-      )
+app.use(function (req, res) {
+  res.status(404).json({
+    errors: [
+      {
+        value: `${req.originalUrl}`,
+        msg: 'A rota ${req.originalUrl} não existe 🚫',
+        param: 'routes'
+      }
+    ]
+  }
+  )
 })
 
 app.listen(port, function () {
