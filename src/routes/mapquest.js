c6ff3d8f5c11dea9add5ff41c0238f805f2951b6
Ricardo Leme <ricardo.leme@gmail.com>
2021-11-29 19:08:06 -0300

    Add geo reverse search

--- src/routes/mapquest.js
+++ src/routes/mapquest.js
@@ -8,11 +8,11 @@ const { MAP_QUEST_API_KEY } = process.env
 
 const router = express.Router()
 
-/**********************************************
- * GET /api/mapquest
- * Retorna o endereço a partir da latitude e longitude informados
- **********************************************/
-router.get('/', async (req, res) => {
+/*****************************************************************************
+ * GET /api/mapquest/geo-reverse
+ * Retorna o endereço/localização a partir da latitude e longitude informados
+  ****************************************************************************/
+router.get('/geo-reverse', async (req, res) => {
   const {lat, lng} = req.query
   try {
     let url = `http://www.mapquestapi.com/geocoding/v1/reverse?key=${MAP_QUEST_API_KEY}&location=${lat},${lng}`
@@ -26,6 +26,31 @@ router.get('/', async (req, res) => {
         {
           value: `${err.message}`,
           msg: 'Erro ao obter o endereço na API MapQuest',
+          param: '/'
+        }
+      ]
+    })
+  }
+})
+
+/*****************************************************************************
+ * GET /api/mapquest/geo
+ * Retorna a latitude e longitude a partir de um endereço/localização informado
+  ****************************************************************************/
+router.get('/geo', async (req, res) => {
+  const {localizacao} = req.query
+  try {
+    let url = `http://www.mapquestapi.com/geocoding/v1/address?key=${MAP_QUEST_API_KEY}&location=${localizacao},BR`
+
+    const endereco = await fetch(url)
+    const dadosEndereco = await endereco.json()
+    res.status(200).json(dadosEndereco.results[0])
+  } catch (err) {
+    res.status(500).json({
+      errors: [
+        {
+          value: `${err.message}`,
+          msg: 'Erro ao obter a latitude e longitude na API MapQuest',
           param: '/'
         }
       ]
