147ea0b641e2eea09249aa3a956fdd6a4a2425dd
Prof. Ricardo Leme <ricardo.leme@fatec.sp.gov.br>
2022-05-03 10:10:53 -0300

    Salvando dados do upload no MongoDB

--- src/routes/upload.js
+++ src/routes/upload.js
@@ -1,8 +1,12 @@
 import express from 'express'
 const router = express.Router()
+// Multer
 import multer from 'multer'
 import * as path from 'path'
-
+// Collection
+import { connectToDatabase } from '../utils/mongodb.js'
+const nomeCollection = 'uploads'
+const { db, ObjectId } = await connectToDatabase()
 
 //Configurações do Multer (Pasta padrão e como será gravado o nome do arquivo)
 const multerStorage = multer.diskStorage({
@@ -10,16 +14,18 @@ const multerStorage = multer.diskStorage({
         callback(null, "./public");
     },
     filename: (req, file, callback) => {
-        const ext = file.mimetype.split("/")[1];
-        callback(null, `uploads/dogWalker-${file.originalname}-${Date.now()}.${ext}`);
+        const ext = path.parse(file.originalname).ext 
+        //Adicionando a data atual em segundos no nome do arquivo
+        callback(null, `uploads/dogWalker-${path.parse(file.originalname).name}-${Date.now()}${ext}`);
     },
-});
+})
+
 
 // Efetuando filtros dos tipos de arquivo
 const multerFilter = (req, file, callback) => {
     let ext = path.extname(file.originalname);
     if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-        return callback(new Error('Apenas imagens são aceitas!'))
+        return callback(new Error('⚠️Apenas imagens são aceitas!'))
     }
     else {
         callback(null, true)
@@ -32,8 +38,6 @@ const upload = multer({
     fileFilter: multerFilter,
 })
 
-
-
 /********************************************
 * Processo de upload de uma imagem
 * POST /
@@ -42,23 +46,27 @@ const uploadSingleImage = upload.single('arquivo')
 
 router.post('/', function (req, res) {
 
-    uploadSingleImage(req, res, function (err) {
+    uploadSingleImage(req, res, async function (err) {
         if (err) { //ocorreu algum erro?
             return res.status(400).json({
                 status: false,
-                message: `Não foi possível efetuar o upload: ${err}`,
+                message: `⚠️Não foi possível efetuar o upload: ${err}`,
             })
         }
-        const file = req.file;
-        res.status(200).send({
+        const file = req.file
+        const dadosArquivo = {
             status: true,
             filename: file.filename,
             mimetype: file.mimetype,
             originalname: file.originalname,
             size: file.size,
             fieldname: file.fieldname
-        })
-
+        }
+        //Salvando os dados do upload no MongoDB
+        await db.collection(nomeCollection)
+        .insertOne(dadosArquivo)
+        .then(result => res.status(200).send(dadosArquivo)) 
+        .catch(err => res.status(400).json(err))
     })
 })
 
